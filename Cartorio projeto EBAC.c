f85c4f2cd63917408ec46eaac8f4019f58782bdc
Renansecol <140573276+Renansecol@users.noreply.github.com>
2023-07-27 19:26:43 -0300

    Opção de saída
    Incluso no sistema a opção de saída do usuário.

--- Cartorio projeto EBAC.c
+++ Cartorio projeto EBAC.c
@@ -125,6 +125,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar os nomes\n");
 		printf("\t3 - Deletar os nomes\n\n"); 
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção: "); //fim do menu
 	
 		scanf("%d", &opcao); //armazenando a escolha do usuario
@@ -144,6 +145,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
 			break;
 			
 			default:
